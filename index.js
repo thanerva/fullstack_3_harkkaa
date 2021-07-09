6a703adf79ac8abfa8b54883d3de7b928a38ef13
thanerva <bgj170@myy.haaga-helia.fi>
2021-07-09 21:50:43 +0300

    to heroku and beyond

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
 
+const cors = require("cors");
+
 app.use(express.json());
+app.use(cors());
 
 const requestLogger = (request, response, next) => {
   console.log("Method:", request.method);
@@ -87,7 +90,7 @@ app.post("/api/notes", (request, response) => {
   response.json(note);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
 });
