1024a6ee295625e6f3733f9990bb06f84663763a
thanerva <bgj170@myy.haaga-helia.fi>
2021-07-16 15:03:12 +0300

    some functionalities with db added

--- index.js
+++ index.js
@@ -80,15 +80,9 @@ app.get("/api/notes", (req, res) => {
 });
 
 app.get("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  console.log(id);
-  const note = notes.find((note) => note.id === id);
-
-  if (note) {
+  Note.findById(request.params.id).then((note) => {
     response.json(note);
-  } else {
-    response.status(404).end();
-  }
+  });
 });
 
 app.delete("/api/notes/:id", (request, response) => {
@@ -106,22 +100,21 @@ const generatedId = () => {
 app.post("/api/notes", (request, response) => {
   const body = request.body;
 
-  if (!body.content) {
+  if (body.content === undefined) {
     return response.status(400).json({
       error: "content missing",
     });
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
-    id: generatedId(),
-  };
-  notes = notes.concat(note);
+  });
 
-  console.log(note);
-  response.json(note);
+  note.save().then((savedNote) => {
+    response.json(savedNote);
+  });
 });
 
 const PORT = process.env.PORT;
