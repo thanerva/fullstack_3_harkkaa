0bbee49a44ee9f50df19e6114c87c068dd17ca3d
thanerva <bgj170@myy.haaga-helia.fi>
2021-08-06 17:10:00 +0300

    uibuild

--- models/note.js
+++ models/note.js
@@ -1,24 +1,11 @@
 const mongoose = require("mongoose");
 
-const url = process.env.MONGODB_URI;
-
-console.log("connecting to", url);
-mongoose
-  .connect(url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-  })
-  .then((result) => {
-    console.log("connected to MongoDB");
-  })
-  .catch((error) => {
-    console.log("error connecting to MongoDB:", error.message);
-  });
-
 const noteSchema = new mongoose.Schema({
-  content: String,
+  content: {
+    type: String,
+    minlength: 5,
+    required: true,
+  },
   date: Date,
   important: Boolean,
 });
